8fd1b89c250d9ea8c4e48fe68f90980a461634d6
Doken1331 <lass245@mail.ru>
2021-09-15 03:48:53 +0300

    add jump count

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
+let jump_html = document.getElementById('jump');
+let jump_count = 0;
 
 document.addEventListener("keydown", function(event) {
     jump();
@@ -8,6 +10,7 @@ document.addEventListener("keydown", function(event) {
 function jump() {
     if (dino.classList != "jump") {
         dino.classList.add("jump");
+        jump_html.innerHTML = `Jump count: ${jump_count += 1}`;
     }
     setTimeout(function() {
         dino.classList.remove("jump");
@@ -18,6 +21,7 @@ let isAlive = setInterval(function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
     if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
-        alert("GAME OVER!");
+        alert(`GAME OVER! \n\nJump count: ${jump_count}`);
+        jump_html.innerHTML = `Jump count: ${jump_count = 0}`;
     }
 }, 10)
